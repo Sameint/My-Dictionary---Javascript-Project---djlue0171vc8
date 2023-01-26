982d56bf53f1f0a941c4c57b1d5e983b5793c2a0
Ram Narayan <shukla.narayanram@gmail.com>
2023-01-26 23:51:11 +0530

    html css js done

--- App.js
+++ App.js
@@ -4,19 +4,41 @@
 //     "resolution": "You can try the search again at later time or head to the web instead."
 //   }
 
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn1 = document.getElementById("search-btn");
+const btn2 = document.getElementById("history-btn");
 
-const url='https://api.dictionaryapi.dev/api/v2/entries/en/hello';
-const searchBtn=document.getElementById('search');
-const historyBtn=document.getElementById('history');
-const input=document.getElementById('search-bar');
-
-async function searchInput(){
-const data=await fetch(url).then((res) => res.json()).then((data) =>{
-    console.log(data);
-})
-data()
-}
-
-function searchHistory(){
-
+btn1.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+});
+function playSound() {
+    sound.play();
 }
