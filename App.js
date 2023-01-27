f257ee697b2fae3ed632f13227b39e677718a0ce
Ram Narayan <shukla.narayanram@gmail.com>
2023-01-28 00:32:30 +0530

    delete btn add

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn1 = document.getElementById("search-btn");
 const btn2 = document.getElementById("history-btn");
-
+const  btn3=document.getElementById("delete");
 btn1.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
     fetch(`${url}${inpWord}`)
@@ -19,26 +19,42 @@ btn1.addEventListener("click", () => {
             result.innerHTML = `
             <div class="word">
                     <h3>${inpWord}</h3>
+                    
                     <button onclick="playSound()">
                         <i class="fas fa-volume-up"></i>
                     </button>
+                    
                 </div>
                 <div class="details">
                     <p>${data[0].meanings[0].partOfSpeech}</p>
                     <p>/${data[0].phonetic}/</p>
                 </div>
+                
                 <p class="word-meaning">
                    ${data[0].meanings[0].definitions[0].definition}
                 </p>
                 <p class="word-example">
                     ${data[0].meanings[0].definitions[0].example || ""}
-                </p>`;
+                </p>
+               `;
+               
             sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         })
         .catch(() => {
             result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
         });
 });
+
 function playSound() {
     sound.play();
 }
+
+
+btn3.addEventListener('click',() =>{
+   
+if(result.innerText!==''){
+    result.innerText='';
+
+}
+})
+
