31dbff7332c15fa9562ddedde0799beff52e8e04
Ram Narayan <shukla.narayanram@gmail.com>
2023-03-12 01:01:07 +0530

    history

--- App.js
+++ App.js
@@ -1,31 +1,24 @@
-// {
-//     "title": "Something Went Wrong",
-//     "message": "Sorry pal, something went wrong, and its not your fault.",
-//     "resolution": "You can try the search again at later time or head to the web instead."
-//   }
-
-const url ="https://api.dictionaryapi.dev/api/v2/entries/en/";
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const btn1 = document.getElementById("search-btn");
 const btn2 = document.getElementById("history-btn");
-const btn3 = document.createElement("button");
 
 btn1.addEventListener("click", () => {
-    let inpWord = document.getElementById("inp-word").value;
-    fetch(`${url}${inpWord}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            var oldData=localStorage.getItem('history');
-            if(oldData){
-                oldData=JSON.parse(oldData);
-                oldData.push(data);
-            }else{
-                oldData=[];
-                oldData.push(data);
-            }
-            localStorage.setItem('history',JSON.stringify(oldData));
-           result.innerHTML = `
+  var inpWord = document.getElementById("inp-word").value;
+  fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var oldData = localStorage.getItem("history");
+      if (oldData) {
+        oldData = JSON.parse(oldData);
+        oldData.push(data);
+      } else {
+        oldData = [];
+        oldData.push(data);
+      }
+      localStorage.setItem("history", JSON.stringify(oldData));
+      result.innerHTML = `
              <div class="word">
                     <h3>${inpWord}</h3>
             </div>
@@ -41,32 +34,42 @@ btn1.addEventListener("click", () => {
                     ${data[0].meanings[0].definitions[0].example || ""}
                 </p> 
                 <br>  <button class="delete" onclick="dele()" style="color:red;margin-left:80%">Delete</button> `;
-              
-          })
-        .catch(() => {
-            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>
-            <button class="delete" onclick="dele()" style="color:red; margin-left:80%">Delete</button>`
-            
-        });
+    })
+    .catch(() => {
+      result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>
+            <button class="delete" onclick="dele()" style="color:red; margin-left:80%">Delete</button>`;
+    });
 });
-function dele(){
-    if(result.innerHTML!=''){
-        result.innerHTML='';
-        localStorage.removeItem('word');
-    }
-}
-btn2.addEventListener('click' ,() =>{
-    var getitem=localStorage.getItem('history');
-    if(getitem){
-        getitem=JSON.parse(getitem);
-    }else{
-        getitem=[];
-        
+
+function dele() {
+  if (result.innerHTML != "") {
+    result.innerHTML = "";
+    localStorage.removeItem("word");
   }
-console.log(getitem);
-})
-
-
-    
-
+}
+btn2.addEventListener("click", () => {
+  var inpWord = document.getElementById("inp-word").value;
+   var allDiv = "";
+  var old = localStorage.getItem("history");
+  old= JSON.parse(old);
+  old.forEach((data) => {
+    allDiv = allDiv +
+      `<div class="word">
+      <h3>${inpWord}</h3>
+                </div>
+                <div class="details">
+                    <p>${data[0]}</p>
+                    <p>/${data[0]}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data.meanings}
+                </p>
+               
+                <p class="word-example">
+                    ${data || ""}
+                </p> 
+                <br>  <button class="delete" onclick="dele()" style="color:red;margin-left:80%">Delete</button> `;
+  });
+  result.innerHTML = allDiv;
+});
 
